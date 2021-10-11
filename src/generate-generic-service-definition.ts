906d31b56a06af6998a1b8187fb2fe1768fc4b72
Kyle Maxwell <kyle@kylemaxwell.com>
2021-10-11 15:01:54 -0700

    no ServiceDefinitions for now

--- src/generate-generic-service-definition.ts
+++ src/generate-generic-service-definition.ts
@@ -49,7 +49,7 @@ export function generateGenericServiceDefinition(
   chunks.push(code`
       },
     } as const;
-    ServiceDefinitions.push(${def(`${serviceDesc.name}Definition`)})
+    //ServiceDefinitions.push(${def(`${serviceDesc.name}Definition`)})
   `);
 
   return joinCode(chunks, { on: '\n' });
